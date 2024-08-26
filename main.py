33afcf52a214798ebe244ea1184406b054a36dd7
Guilherme Figueiredo <168232647+GuilhermeDev217@users.noreply.github.com>
2024-08-26 08:46:35 -0300

    main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+numeroInteiro: int #inteiro
+numeroDecima: float #ponto flutuante
+alfaNum: str #alfa numerico
+falsoouverdadei: bool #Booleano
+
+print ("Ola!!!") #Imprime na tela
+
+numeroInteiro = 10 # atribuição de valor
+numeroDecima = 10.5
+alfaNum = "Bom dia"
+
+print(numeroDecima)
+print(numeroInteiro)
+print(alfaNum)
